aff32d9a784d2280940fe29271425c3486e25ef4
ckrapu <ckrapu@gmail.com>
2019-05-20 13:01:13 -0400

    Adding boilerplate Python files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='lidisa',
+      version='0.1',
+      description='Lightweight implementation of direct sampling',
+      author='Christopher Krapu',
+      author_email='ckrapu@gmail.com',
+      modules=['lidisa']
+     )
