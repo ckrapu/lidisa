0e6f0d45e7b23704ff1785c925ad4621b3e47d84
ckrapu <ckrapu@gmail.com>
2019-05-20 14:00:36 -0400

    Rearranging files again to fix import

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
-
+from setuptools import setup, find_packages
 setup(name='lidisa',
       version='0.1',
       description='Lightweight implementation of direct sampling',
       author='Christopher Krapu',
       author_email='ckrapu@gmail.com',
-      modules=['lidisa']
+      packages=find_packages()
      )
